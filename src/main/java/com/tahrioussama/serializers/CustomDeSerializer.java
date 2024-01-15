f59a00b9f9b4dd2845a9942badcfa64f3e5c6d98
oussama-tahri <tahrioussama.ot@gmail.com>
2024-01-15 01:07:09 +0100

    kafka-spring-cloud-streams

---
+++ src/main/java/com/tahrioussama/serializers/CustomDeSerializer.java
@@ -0,0 +1,17 @@
+package com.tahrioussama.serializers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tahrioussama.entities.PageEvent;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class CustomDeSerializer implements Deserializer<PageEvent> {
+    @Override
+    public PageEvent deserialize(String s, byte[] data) {
+        try {
+            return new ObjectMapper().readValue(new String(data),PageEvent.class);
+        } catch (Exception e) {
+            throw new SerializationException(e);
+        }
+    }
+}
